cb901d7b80f6715a6ef4eca0e71ff51eed1cf13b
Sun Qin <sqlxx@163.com>
2021-07-24 18:02:00 +0800

    first commit

---
+++ pandas_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 24 17:58:12 2021
+
+@author: sqlxx
+"""
+
+import pandas as pd
+
+df1 = pd.DataFrame(dict(x=["a", "b", "c"], y=range(1,4))) 
+print(df1)
